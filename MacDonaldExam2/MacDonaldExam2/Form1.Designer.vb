1c34a1b27a7ba3eec20ea57771c118152f965aa2
GaryHostt <34657980+GaryHostt@users.noreply.github.com>
2018-04-18 14:19:06 -0400

    Uploading all files

---
+++ MacDonaldExam2/MacDonaldExam2/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lstTimes = New System.Windows.Forms.ListBox()
+        Me.btnTimes = New System.Windows.Forms.Button()
+        Me.lblAverageTime = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Berlin Sans FB", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(29, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(314, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Dixon County Corn Maze Time!"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(653, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuClear, Me.mnuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(152, 22)
+        Me.mnuClear.Text = "Clear"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(152, 22)
+        Me.mnuExit.Text = "Exit"
+        '
+        'lstTimes
+        '
+        Me.lstTimes.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstTimes.FormattingEnabled = True
+        Me.lstTimes.Location = New System.Drawing.Point(211, 94)
+        Me.lstTimes.Name = "lstTimes"
+        Me.lstTimes.Size = New System.Drawing.Size(120, 147)
+        Me.lstTimes.TabIndex = 2
+        '
+        'btnTimes
+        '
+        Me.btnTimes.BackColor = System.Drawing.Color.LightYellow
+        Me.btnTimes.Font = New System.Drawing.Font("Microsoft YaHei", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTimes.ForeColor = System.Drawing.Color.DarkBlue
+        Me.btnTimes.Location = New System.Drawing.Point(46, 132)
+        Me.btnTimes.Name = "btnTimes"
+        Me.btnTimes.Size = New System.Drawing.Size(120, 23)
+        Me.btnTimes.TabIndex = 3
+        Me.btnTimes.Text = "Enter times"
+        Me.btnTimes.UseVisualStyleBackColor = False
+        '
+        'lblAverageTime
+        '
+        Me.lblAverageTime.AutoSize = True
+        Me.lblAverageTime.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAverageTime.Location = New System.Drawing.Point(6, 289)
+        Me.lblAverageTime.Name = "lblAverageTime"
+        Me.lblAverageTime.Size = New System.Drawing.Size(348, 26)
+        Me.lblAverageTime.TabIndex = 4
+        Me.lblAverageTime.Text = "Average time of your team is XX.X "
+        Me.lblAverageTime.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(370, 36)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(271, 242)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(7, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(198, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Please enter your time in minutes."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Berlin Sans FB", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(89, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(194, 16)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Team Average time calculator"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(653, 359)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblAverageTime)
+        Me.Controls.Add(Me.btnTimes)
+        Me.Controls.Add(Me.lstTimes)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Race Through the Corn Maze!"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents lstTimes As ListBox
+    Friend WithEvents btnTimes As Button
+    Friend WithEvents lblAverageTime As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ MacDonaldExam2/MacDonaldExam2/Form1.vb
@@ -0,0 +1,83 @@
+﻿'Corn Maze Average time
+' Alex MacDonald
+' October 26, 2017
+'Exam 2
+'Purpose: Enter times for all teammates, up to 10, and display average time for team.
+
+Option Strict On
+
+Public Class Form1
+    Private Sub mnuClear_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+        'This click event clears t he listbox object and hides the average time label, it also enables the weight loss button
+        lstTimes.Items.Clear()
+        lblAverageTime.Visible = False
+        btnTimes.Enabled = True
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        'The mnuExit click event closes the window and exits the application
+        Close()
+    End Sub
+
+    Private Sub btnTimes_Click(sender As Object, e As EventArgs) Handles btnTimes.Click
+        'Accepts up to 10 time values
+        'and then calculates and ddisplays the average time for the team
+        'Decleare and initialize variables
+        Dim strTime As String
+        Dim decTime As Decimal
+        Dim decAverageTime As Decimal
+        Dim decTotalTime As Decimal = 0D
+        Dim strInputMessage As String = "Enter the weight loss for team member #"
+        Dim strInputHeading As String = "Time"
+        Dim strNormalMessage As String = "Enter the time for team member #"
+        Dim strNonNumericError As String = "Error - enter a number for the weight loss of team member #"
+        Dim strNegativeError As String = "Error - Enter a positive number for the time of team member #"
+
+        'dECLARE AND INITIALIZE LOOP VARIABLES
+
+        Dim strCancelClicked As String = ""
+        Dim intMaxNumberOfEntries As Integer = 10
+        Dim intNumberOfEntries As Integer = 1
+
+        'This loop allows the user to enter the weight loss of up to 10 team members.
+        'The loop terminates when the user has entered 10 time values or the user
+        'taps or clicks the cancel button or the close button in the inputbox
+
+        strTime = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+
+        Do Until intNumberOfEntries > intMaxNumberOfEntries Or strTime = strCancelClicked
+
+            If IsNumeric(strTime) Then
+                decTime = Convert.ToDecimal(strTime)
+                If decTime > 0 Then
+                    lstTimes.Items.Add(decTime)
+                    decTotalTime += decTime
+                    intNumberOfEntries += 1
+                    strInputMessage = strNormalMessage
+                Else
+                    strInputMessage = strNegativeError
+                End If
+            Else
+                strInputMessage = strNonNumericError
+            End If
+
+            If intNumberOfEntries <= intMaxNumberOfEntries Then
+                strTime = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+
+            End If
+        Loop
+
+        'Calculates and displays average team time
+        If intNumberOfEntries > 1 Then
+            lblAverageTime.Visible = True
+            decAverageTime = decTotalTime / (intNumberOfEntries - 1)
+            lblAverageTime.Text = "Average time for the team is " &
+                decAverageTime.ToString("F1") & " minutes"
+        Else
+            MsgBox("No time value entered")
+        End If
+
+        'disables the time value button
+        btnTimes.Enabled = False
+    End Sub
+End Class
